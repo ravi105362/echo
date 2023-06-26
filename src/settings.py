1b3b1f3adb68c6d17473217acb6a836fd2f95871
Ravi Gupta <ravigupta.mnnit@gmail.com>
2023-06-26 12:44:27 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -4,5 +4,5 @@ config = Config(".env")
 DB_URI = "postgres://avnadmin"
 LOGGER_FOLDER = config("LOGGER_FOLDER", cast=str, default="src/logs/")
 DATABASE_LOCAL = True
-POSTGRES_DATABASE_URL = 'postgresql+psycopg2://ravi:ravi12304@postgres.ci97v9vjjjf2.eu-central-1.rds.amazonaws.com:5432/postgres'  # noqa: E501
+POSTGRES_DATABASE_URL = 'postgresql+psycopg2://<URL>'  # noqa: E501
 SQLALCHEMY_DATABASE_URL = "sqlite:///endpoints.db"
